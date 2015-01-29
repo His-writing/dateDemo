a72f00cbb3314791b44bf0cbbdec3a1997a48d21
shuzhenguo <shuzhenguo@shuzhenguodeMacBook-Pro-2.local>
2015-01-29 17:21:50 +0800

    Initial Commit

---
+++ dateDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dateDemo
+//
+//  Created by shuzhenguo on 15-1-29.
+//  Copyright (c) 2015年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dateDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dateDemo
+//
+//  Created by shuzhenguo on 15-1-29.
+//  Copyright (c) 2015年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
